5c51af83e39ac539608f7cb64b907ccd7eb5aff8
paulohique <paulohenriquebq2011@hotmail.com>
2024-06-14 14:56:04 -0300

    dsds

--- krib/src/Main.java
+++ krib/src/Main.java
@@ -5,12 +5,13 @@ public class Main {
     
 
     cachorro cachorro1 = new cachorro();
+    cachorro cachorro2 = new cachorro("Puppy","Rosa", 13 ,10 ,10,"normal");
 
     cachorro1.setNome("Zeruela"); 
-    cachorro1.altura = 24;
-    cachorro1.cor = "Vermelho";
-    cachorro1.tamanhoDoRabo = 10;
-    cachorro1.peso = 5.5;
+    cachorro1.setAltura(24);
+    cachorro1.setCor("Vermelho");
+    cachorro1.setPeso( 10);
+    cachorro1.setPeso(5.5);
 
     cachorro1.latir();
     cachorro1.pegar();
@@ -18,5 +19,7 @@ public class Main {
     System.out.println("O cachorro está " + cachorro1.interagir("carinho"));
     System.out.println("O cachorro está " + cachorro1.interagir("dormir"));
     System.out.println("O cachorro está " + cachorro1.interagir("alimentar "));
+
+    System.out.println(cachorro2.getNome());
  }
 }
--- krib/src/animais/cachorro.java
+++ krib/src/animais/cachorro.java
@@ -1,12 +1,31 @@
 package animais;
 
 public class cachorro {
+    // ATRIBUTOS
     private String nome;
     private String cor;
     private int altura;
     private double peso;
     private int tamanhoDoRabo;
     private String estadoEspirito;
+
+
+    // CONSTRUTORES 
+    public cachorro(){}// padrão
+    //  adicionados
+    public cachorro(String nome, String cor, int altura, double peso, int tamanhoDoRabo, String estadoEspirito){
+        this.nome = nome;
+        this.cor = cor;
+        this.altura = altura;
+        this.peso = peso;
+        this.tamanhoDoRabo = tamanhoDoRabo;
+        this.estadoEspirito = estadoEspirito;
+    };
+
+
+
+
+    // METODOS
     // Get e Set para usar em dados
     public String getNome(){
         return this.nome;
@@ -14,8 +33,32 @@ public class cachorro {
     public void setNome(String nome){
         if(nome.equals("lily"))
         this.nome = null;
-    }
-
+        }
+    public String getCor(String cor){
+        return this.cor;
+        }
+    public void setCor(String cor){
+        this.cor = cor;
+        }
+    public int getAltura(int altura){
+        return this.altura;
+        }
+    public void setAltura(int altura){
+        this.altura = altura;
+        }
+    public double getPeso(double peso){
+        return this.peso;
+        }
+    public void setPeso(double peso){
+        this.peso = peso;
+        }
+    public int getTamanhoDoRabo(int tamanhoDoRabo){
+        return this.tamanhoDoRabo;
+        }
+    public void setPeso(int tamanhoDoRabo){
+        this.tamanhoDoRabo = tamanhoDoRabo;
+        }
+    
     public void  comer() {
     }
     public void latir(){
